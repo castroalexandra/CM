727e10d79ddbbc59e60a2d9d1014528f679c1502
Alexandra Castro <alexandracastro@ipvc.pt>
2021-04-12 21:04:33 +0100

    layout login

--- app/src/main/java/com/example/cm/MainActivity.kt
+++ app/src/main/java/com/example/cm/MainActivity.kt
@@ -9,9 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.notasButton)
-        button.setOnClickListener {
+
+        val notasBtn: Button = findViewById(R.id.notasButton)
+        notasBtn.setOnClickListener {
             val intent = Intent(this@MainActivity, NotasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val mapaBtn: Button = findViewById(R.id.mapaButton)
+        mapaBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
         }
     }
